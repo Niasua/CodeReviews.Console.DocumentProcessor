c637ab6c4f707a6e09e571fb2dd121f8da0f3c82
niasua <nsuarez2410@gmail.com>
2026-01-02 15:22:46 -0300

    adds the excel parser

---
+++ DocumentProcessor.Niasua/DocumentProcessor.Niasua/Parser/ExcelContactParser.cs
@@ -0,0 +1,33 @@
+﻿using ClosedXML.Excel;
+using DocumentProcessor.Niasua.Models;
+
+namespace DocumentProcessor.Niasua.Parser;
+
+internal class ExcelContactParser : IFileParser<Contact>
+{
+    public List<Contact> Parse(string filePath)
+    {
+        using var workbook = new XLWorkbook(filePath);
+
+        // use the first worksheet
+        var worksheet = workbook.Worksheet(1);
+        // for using only the active rows
+        var rows = worksheet.RowsUsed();
+
+        var contacts = new List<Contact>();
+        
+        foreach (var row in rows.Skip(1)) // the first is usually the header, so it's skipped
+        { 
+            var contact = new Contact
+            {
+                Name = row.Cell(1).GetValue<string>(),
+                Email = row.Cell(2).GetValue<string>(),
+                PhoneNumber = row.Cell(3).GetValue<string>()
+            };
+
+            contacts.Add(contact);
+        }
+
+        return contacts;
+    }
+}
