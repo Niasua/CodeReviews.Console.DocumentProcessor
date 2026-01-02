79627c53cc32b4c6d51d7773d144e3da63b2b417
niasua <nsuarez2410@gmail.com>
2026-01-02 15:38:08 -0300

    adds a factory to decoyple logic from program.cs

---
+++ DocumentProcessor.Niasua/DocumentProcessor.Niasua/Parser/FileParserFactory.cs
@@ -0,0 +1,20 @@
+﻿using DocumentProcessor.Niasua.Models;
+
+namespace DocumentProcessor.Niasua.Parser;
+
+internal class FileParserFactory
+{
+    public static IFileParser<Contact> GetParser(string filePath)
+    {
+        // get extension
+        var extension = Path.GetExtension(filePath).ToLower();
+
+        // decide which parser to use
+        return extension switch
+        {
+            ".csv" => new CsvContactParser(),
+            ".xlsx" => new ExcelContactParser(),
+            _ => throw new NotSupportedException($"Format {extension} is not supported!")
+        };
+    }
+}
