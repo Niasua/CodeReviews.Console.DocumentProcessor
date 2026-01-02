543d5a2ffc7f2ca487b022e380fafc29df1dacdc
niasua <nsuarez2410@gmail.com>
2026-01-01 23:01:57 -0300

    contact model created

---
+++ DocumentProcessor.Niasua/DocumentProcessor.Niasua/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace DocumentProcessor.Niasua.Models;
+
+internal class Contact
+{
+    [Ignore]
+    public int Id { get; set; }
+
+    [Name("First Name")]
+    public string Name { get; set; }
+
+    [Name("Email Address")]
+    public string Email { get; set; }
+
+    [Name("Phone")]
+    public string PhoneNumber { get; set; }
+}
