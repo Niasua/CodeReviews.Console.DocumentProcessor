14444518f662ab30606c46aeb0c988a0d7faffdc
niasua <nsuarez2410@gmail.com>
2026-01-02 15:46:49 -0300

    tests the correct functioning of reading csv files

--- DocumentProcessor.Niasua/DocumentProcessor.Niasua/Program.cs
+++ DocumentProcessor.Niasua/DocumentProcessor.Niasua/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DocumentProcessor.Niasua.Parser;
+
+var filePath = "contacts.csv";
+
+var parser = FileParserFactory.GetParser(filePath);
+
+var contacts = parser.Parse(filePath);
+
+Console.WriteLine($"{contacts.Count} were found");
+
+foreach (var contact in contacts)
+{
+    Console.WriteLine(contact.Name);
+}
