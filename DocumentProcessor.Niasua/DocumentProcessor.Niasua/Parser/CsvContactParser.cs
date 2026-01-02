39e96832bf1357486f282da1ade0c97d8ee970c3
niasua <nsuarez2410@gmail.com>
2026-01-01 23:02:58 -0300

    creates concrete parser for csv

---
+++ DocumentProcessor.Niasua/DocumentProcessor.Niasua/Parser/CsvContactParser.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper;
+using DocumentProcessor.Niasua.Models;
+using System.Globalization;
+
+namespace DocumentProcessor.Niasua.Parser;
+
+internal class CsvContactParser : IFileParser<Contact>
+{
+    public List<Contact> Parse(string filePath)
+    {
+        using var reader = new StreamReader(filePath);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+        var records = csv.GetRecords<Contact>();
+
+        return new List<Contact>(records);
+    }
+}
